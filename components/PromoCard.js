eab46c028c8f0aa785f1587927ee4007b7ce5c8f
joedubazilio2017-ttt <jo.edubazilio2017@gmail.com>
2026-08-26 22:41:53 -0300

    Update PromoCard.js

--- components/PromoCard.js
+++ components/PromoCard.js
@@ -1,7 +1,3 @@
-ARQUIVO: components/PromoCard.js
-AÇÃO: SUBSTITUIR o conteúdo do arquivo que já existe nesse caminho
-======================================================================
-
 "use client";
 
 import { useState } from "react";
